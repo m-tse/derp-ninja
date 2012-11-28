9935f0aad3939127daabb783fcd2a264b2fc2bb2
Matthew Tse <matt.s.tse@gmail.com>
2012-11-28 01:12:38 -0500

    added skeleton implementations of abstract classes

---
+++ src/dblockcache/myDBufferCache.java
@@ -0,0 +1,28 @@
+package dblockcache;
+
+public class myDBufferCache extends DBufferCache{
+
+	public myDBufferCache(int cacheSize) {
+		super(cacheSize);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public DBuffer getBlock(int blockID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void releaseBlock(DBuffer buf) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sync() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
