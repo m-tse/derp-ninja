af8e43f4abe22824dc391efff301ae2406e073b2
afacool1 <afacool1@gmail.com>
2012-11-29 02:11:43 -0500

    working on cache

--- src/dblockcache/myDBufferCache.java
+++ src/dblockcache/myDBufferCache.java
@@ -1,28 +1,47 @@
 package dblockcache;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class myDBufferCache extends DBufferCache{
+
+	private final int cacheSize;
+	private HashMap<Integer,DBuffer> cache;
+	private LinkedList<Integer> queue;
 
 	public myDBufferCache(int cacheSize) {
 		super(cacheSize);
-		// TODO Auto-generated constructor stub
+		this.cacheSize=cacheSize;
+		cache= new HashMap<Integer,DBuffer>();
+		queue= new LinkedList<Integer>();
 	}
 
 	@Override
 	public DBuffer getBlock(int blockID) {
-		// TODO Auto-generated method stub
-		return null;
+		//check if blockId is in the cache
+		if(cache.containsKey(blockID))
+		{
+			//update the list:
+			queue.remove(blockID);
+			queue.addLast(blockID);
+			return cache.get(blockID);
+
+		}
+		//remove a block from cache
+		cache.put(queue.removeFirst(),null);
+
 	}
 
 	@Override
 	public void releaseBlock(DBuffer buf) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void sync() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
