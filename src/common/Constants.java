8ecd0b1d9b6d0e39ebc9766c37b4a8cb9f37f0bd
Matthew Tse <matt.s.tse@gmail.com>
2012-11-30 00:48:06 -0500

    beginnig to work on DFS

--- src/common/Constants.java
+++ src/common/Constants.java
@@ -7,6 +7,8 @@ public class Constants {
 
 	public static final int NUM_OF_BLOCKS = 16384; // 2^14
 	public static final int BLOCK_SIZE = 1024; // 1kB
+	public static final int MAX_NUM_FILES = 512;
+	public static final int INODE_SIZE = 256;
 
 	/* DStore Operation types */
 	public enum DiskOperationType {
@@ -16,5 +18,3 @@ public class Constants {
 	/* Virtual disk file/store name */
 	public static final String vdiskName = "DSTORE.dat";
 }
-
-//this is a drity little comment!
--- src/dfs/myDFS.java
+++ src/dfs/myDFS.java
@@ -1,11 +1,35 @@
 package dfs;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import common.Constants;
 import common.DFileID;
+import common.INode;
+import dblockcache.DBufferCache;
+import dblockcache.myDBufferCache;
 
 public class myDFS extends DFS{
-
+	DFileID[] _DFileIDs;
+	INode[] _INodes;
+	DBufferCache _DBufferCache;
+	
+	public myDFS(String volname, boolean format) throws FileNotFoundException, IOException{
+		super(volname, format);
+		_DFileIDs = new DFileID[Constants.MAX_NUM_FILES];
+		_INodes = new INode[Constants.MAX_NUM_FILES];
+		_DBufferCache = new myDBufferCache();
+	}
+	
+	public myDFS(boolean format) throws FileNotFoundException, IOException{
+		this(Constants.vdiskName, format);
+	}
+	
+	public myDFS() throws FileNotFoundException, IOException{
+		this(Constants.vdiskName, false);// should it be initialized to false?
+	}
 	@Override
 	public boolean format() {
 		// TODO Auto-generated method stub
@@ -14,6 +38,7 @@ public class myDFS extends DFS{
 
 	@Override
 	public DFileID createDFile() {
+
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -44,8 +69,9 @@ public class myDFS extends DFS{
 
 	@Override
 	public List<DFileID> listAllDFiles() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<DFileID> dfiles = new ArrayList<DFileID>();
+		for (DFileID s:_DFileIDs) dfiles.add(s);
+		return dfiles;
 	}
 
 }
