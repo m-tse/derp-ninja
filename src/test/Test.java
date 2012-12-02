35ae67f1157db33486f1ce48e2b2a5007084cb38
Matthew Tse <matt.s.tse@gmail.com>
2012-12-01 22:39:17 -0500

    fixed compilation error for Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -13,18 +13,18 @@ public class Test {
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		
 		
-//		MyDFS dfs = new MyDFS();	
-//		String s = "Hello DeFiler World!"; 
-//		byte[] sBytes = s.getBytes();
-//		byte[] rBytes = new byte[sBytes.length];
-//		DFileID myFileID = dfs.createDFile();
-//		dfs.write(myFileID, sBytes, 0, sBytes.length);
-//	
-//		dfs.read(new DFileID(0), rBytes, 0, rBytes.length);
-//		String sp = new String(rBytes);
-//		
-//		System.out.println(sp);
-//		System.out.println("COMPLETE");
+		MyDFS dfs = new MyDFS();	
+		String s = "Hello DeFiler World!"; 
+		byte[] sBytes = s.getBytes();
+		byte[] rBytes = new byte[sBytes.length];
+		DFileID myFileID = dfs.createDFile();
+		dfs.write(myFileID, sBytes, 0, sBytes.length);
+	
+		dfs.read(new DFileID(0), rBytes, 0, rBytes.length);
+		String sp = new String(rBytes);
+		
+		System.out.println(sp);
+		System.out.println("COMPLETE");
 		
 	}
 	
